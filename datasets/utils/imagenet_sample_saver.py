5a4a61e66b3f01004c53e64b560193e5d0c7bb3f
dfbakin <denis.bakin5@gmail.com>
2025-03-06 16:53:31 +0300

    feat: added chosen datasets feat: added utils for saving imagenet samples

---
+++ datasets/utils/imagenet_sample_saver.py
@@ -0,0 +1,28 @@
+from nltk.corpus import wordnet
+import os
+import random
+
+
+def copy_chosen(source, destination, samples_n=50, cnt=-1):
+    for filename in random.sample(os.listdir(current_class_folder), samples_n):
+        # print(f"cp {os.path.join(current_class_folder, filename)} {os.path.join(base_dist_path, dirname)}")
+        os.system(f"cp {os.path.join(current_class_folder, filename)} {os.path.join(base_dist_path, dirname)}")
+    print(f"Done with {dirname} \t #{cnt}")
+
+base_source_path = ""
+base_dist_path = ""
+
+classes_n = len(os.listdir(base_source_path))
+print(f"Total of {classes_n} classes")
+
+i = 1
+threads = []
+for dirname in os.listdir(base_source_path):
+    class_dist = os.path.join(base_dist_path, dirname)
+    if not os.path.exists(class_dist):
+        os.mkdir(class_dist)
+    current_class_folder = os.path.join(base_source_path, dirname)
+    copy_chosen(current_class_folder, class_dist, 50000 // classes_n, i)
+    i += 1
+
+    
